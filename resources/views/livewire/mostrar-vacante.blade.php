65f10ba8bea05ffd009c63c4cb763bb0859acca2
Jose Quiñónez <joserobertoq@outlook.com>
2023-06-09 05:19:42 -0600

    Mostrando imagen y descripción del puesto

--- resources/views/livewire/mostrar-vacante.blade.php
+++ resources/views/livewire/mostrar-vacante.blade.php
@@ -22,4 +22,15 @@
             </p>
         </div>
     </div>
+
+    <div class="md:grid md:grid-cols-6 bg-gray-50 p-4 gap-4">
+        <div class="md:col-span-2">
+            <img src="{{asset('storage/vacantes/' . $vacante->imagen)}}" alt="{{'Imagen vacante ' . $vacante->titulo}}">
+        </div>
+
+        <div class="md:col-span-4">
+            <h2 class="text-2xl font-bold mb-5">Descripción del puesto</h2>
+            <p>{{$vacante->descripcion}}</p>
+        </div>
+    </div>
 </div>
