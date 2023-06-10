c279934220d7b338c2356df94c06d2a7451f90ed
Jose Quiñónez <joserobertoq@outlook.com>
2023-06-10 06:01:43 -0600

    Creando migración de Candidatos

--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -3,8 +3,12 @@
 
     <form class="w-96 mt-5">
         <div class="mb-4">
-            <x-input-label for="cv" :value="__('Curriculum u Hoja de Vida (PDF)')"></x-input-label>
-            <x-text-input id="cv" type="file" accept="pdf" class="block mt-1 w-full"></x-text-input>
+            <x-input-label class="mb-4" for="cv" :value="__('Curriculum u Hoja de Vida (PDF)')"></x-input-label>
+            <x-text-input id="cv" type="file" accept=".pdf" class="block mt-1 w-full"></x-text-input>
         </div>
+
+        <x-primary-button class="my-5 w-full justify-center">
+            {{__('Postularme')}}
+        </x-primary-button>
     </form>
 </div> 
