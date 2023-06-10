1d86672f74e51e3489418a229fa844064f473165
Jose Quiñónez <joserobertoq@outlook.com>
2023-06-10 06:12:14 -0600

    Agregando validación del formulario

--- app/Http/Livewire/PostularVacante.php
+++ app/Http/Livewire/PostularVacante.php
@@ -6,6 +6,27 @@ use Livewire\Component;
 
 class PostularVacante extends Component
 {
+    public $cv;
+
+    protected $rules = [
+        'cv' => 'required|mimes:pdf'
+    ];
+
+    public function postularme()
+    {
+        $this->validate();
+
+        // Almacenar el CV
+
+        // Crear Vacantes
+
+        // Crear la notificación y enviar el email
+ 
+        // Mostrar al usuario un mensaje de ok
+
+        
+    }
+
     public function render()
     {
         return view('livewire.postular-vacante');
