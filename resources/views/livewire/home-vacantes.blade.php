959d77b08f103dfd13707fb1f1a93608452d9f52
Jose Quiñónez <joserobertoq@outlook.com>
2023-06-15 18:55:56 -0600

    Creando componente de buscador

--- resources/views/livewire/home-vacantes.blade.php
+++ resources/views/livewire/home-vacantes.blade.php
@@ -1,4 +1,5 @@
 <div>
+    <livewire:filtrar-vacantes>
     <div class="p-12">
         <div class="max-w-7xl mx-auto">
             <h3 class="font-extrabold text-4xl text-gray-800 mb-12">Nuestras Vacantes Disponibles</h3>
