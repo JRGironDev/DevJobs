5f1145880404d0500ced8de6b6829421a5ced918
Jose Quiñónez <joserobertoq@outlook.com>
2023-06-15 21:57:39 -0600

    Completar filtro

--- app/Http/Livewire/HomeVacantes.php
+++ app/Http/Livewire/HomeVacantes.php
@@ -25,8 +25,20 @@ class HomeVacantes extends Component
         //$vacantes = Vacante::all();
 
         $vacantes = Vacante::when($this->termino, function($query){
-            $query->where('titulo', 'LIKE', "%" . $this->termino . '%');
-        })->paginate(20);
+            $query->where('titulo', 'LIKE', "%" . $this->termino . "%");
+        })
+        ->when($this->termino, function($query){
+            $query->orWhere('empresa', 'LIKE', "%" . $this->termino . "%");
+        })
+        ->when($this->categoria, function($query){
+            $query->where('categoria_id', $this->categoria);
+        })
+        ->when($this->salario, function($query){
+            $query->where('salario_id', $this->salario);
+        })
+        ->paginate(20);
+
+
 
         return view('livewire.home-vacantes', [
             'vacantes' => $vacantes
